5938c28d3cd2b2854d850b925445f2d00bf0d412
jayan <jayududakiya@gmail.com>
2023-07-21 17:18:48 +0530

    Swap Two Array Elements

--- Array_sorting_Type2.c
+++ Array_sorting_Type2.c
@@ -3,20 +3,27 @@
 // 50 , 20 , 30 , 40 , 10 
 //  4    1   2    3    0
 
+#define n 5
 
 int main ()
 {
-    int A[5]={10,20,30,40,50},i=0;
+    int A[n],i,temp;
 
-    do
+    for (i=0;i<n;i++)
     {
-        printf("\n [%d] : %d ",i,A[i+4]);
-        printf("\n [%d] : %d ",i,A[i+1]);
-        printf("\n [%d] : %d ",i,A[i+2]);
-        printf("\n [%d] : %d ",i,A[i+3]);
-        printf("\n [%d] : %d ",i,A[i]);
-        i++;
-    }while(i==0);
+        printf("\n Enter A :");
+        scanf("%d",&A[i]);
+    }
 
-    
+    for (i=0;i<n;i++)
+    {
+        if(i==0)
+     {
+       temp=A[0];
+       A[0]=A[n-1];
+       A[n-1]=temp;
+     }  
+       printf(" %d ",A[i]);
+    }
+
 }
