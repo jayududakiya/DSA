2868df6660b322b56ada3569565a7f24cce35a41
jayan <jayududakiya@gmail.com>
2023-07-21 16:58:54 +0530

    Merge Tow Array

--- Tow_array.c
+++ Tow_array.c
@@ -1,21 +1,48 @@
 #include<stdio.h>
 
-int main ()
+int main()
 {
-
-    int m=5;
-    int A[5]={1,2,3,4,5} , B[5]={6,7,8,9,10} , marge[10] ,i;
-
-     for(i=0;i<m;i++)
-     {
-        marge[i]=A[i]; 
-        marge[i+m]=B[i];  
-     }
+    int m=3,n=5,i,k;
      
-     printf("\n Merge Array : ");
-     for (i=0;i<10;i++)
-     {
-        printf(" %d ",marge[i]);
-     }
+     k=m+n;
+
+    int A[m],B[n],marge[k];
+
+    // printf("\n Enter size of Array  A ");
+    // scanf("%d",&m);
+
+    for(i=0;i<m;i++)
+    {
+        printf("Enter A : ");
+        scanf("%d",&A[i]);
+    }
+ 
+     printf("\n");
      
+    // printf("\n Enter size of Array  B ");
+    // scanf("%d",&n);
+   
+
+    for(i=0;i<n;i++)
+    {
+        printf("Enter B : ");
+        scanf("%d",&B[i]);
+    }
+
+
+    for (i=0;i<m;i++)
+    {
+        marge[i]=A[i];
+    }
+
+    for (i=0;i<n;i++)
+    {
+        marge[i+m]=B[i];
+    }
+
+    for (i=0;i<k;i++)
+    {
+        printf("\n [%d] %d ",i,marge[i]);
+    }
+
 }
